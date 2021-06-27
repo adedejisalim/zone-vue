e6b0fb999224f6a5912f7bb363205aca47450744
salim <s534027@nwmissouri.edu>
2021-06-27 15:18:28 -0500

    changed api keys

--- src/db.js
+++ src/db.js
@@ -3,12 +3,12 @@ import "firebase/database";
 
 const config = {
     //API KEYS
-    apiKey: "AIzaSyDmvIHyH89SLUql2yN9hwkogY4APWfsdTc",
-    authDomain: "zonechat-c4b06.firebaseapp.com",
-    projectId: "zonechat-c4b06",
-    storageBucket: "zonechat-c4b06.appspot.com",
-    messagingSenderId: "967356380101",
-    appId: "1:967356380101:web:9ac1715a24220b922891ac"
+    apiKey: "AIzaSyBzTxo-Em9aFC-G0S7hhIxsmlY9JJOCmF0",
+    authDomain: "zonechat-9d142.firebaseapp.com",
+    projectId: "zonechat-9d142",
+    storageBucket: "zonechat-9d142.appspot.com",
+    messagingSenderId: "1055685397517",
+    appId: "1:1055685397517:web:4650eb1457546a89415944"
 };
 
 const db = firebase.initializeApp(config);
